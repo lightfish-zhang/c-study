7d5f9f45046edbd03b17b1d66c38dc5561186bdd
coolyeah <coolyeah@foxmail.com>
2016-12-21 19:53:26 +0800

    add splay

---
+++ tree/splay/splay.h
@@ -0,0 +1,17 @@
+typedef int keyType;
+
+typedef struct SplayTreeNode{
+  keyType key;
+  struct SplayTreeNode * left;
+  struct SplayTreeNode * right;
+} SplayTreeNode, * SplayTree;
+
+
+SplayTree splaytree_splay( SplayTree tree, keyType key ){
+
+  SplayTreeNode N, *l, *r, *c;
+
+  if(  )
+
+
+}
